4f20ed6bc6c103f52f32277187d8c904baf3ae40
Sherlock-Cheng <59915005+Sherlock-Cheng@users.noreply.github.com>
2021-01-05 22:03:11 +0800

    try to upload the file for the first time

---
+++ 1.py
@@ -0,0 +1,9 @@
+print('--------------------------------')
+temp=input("guess which number i want now:")
+guess=int(temp)
+if guess==8:
+    print("good, you're right")
+    print(" but, just right no wards.")
+else:
+    print("you're wrong.")
+print("game over.")
